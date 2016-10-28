3358cece0188fe5f481f493b1e751a1e765f8746
cbbrowne <cbbrowne@cbbrowne.tor.int-afilias.info>
2016-10-28 17:40:30 -0400

    Issue #9 - Prefer use of URIs to reference databases

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -5,7 +5,8 @@ for db in test1 test2; do
     psql -d ${db} -f schema1.sql
 done
 dropdb comparisondatabase
-
+createdb comparisondatabase
 psql -d test2 -f schema2.sql
 
-DB1=test1 DB2=test2 EXPLANATIONS=./explanations.txt ../pgcmp
+
+URI1=postgresql://postgres@localhost/test1 URI2=postgresql://postgres@localhost/test2 EXPLANATIONS=./explanations.txt ../pgcmp
