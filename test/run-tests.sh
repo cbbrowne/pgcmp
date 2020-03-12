ad19b61f6bf589ca0fdb5a94cd8b18a1f39396ab
cbbrowne <cbbrowne@cbbrowne.tor.int-afilias.info>
2020-03-12 13:49:32 -0400

    Improve parameterization a tad

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 for db in test1 test2; do
-    dropdb $db
-    createdb $db
-    psql -d ${db} -f schema1.sql
+    dropdb --if-exists $db
+    createdb -h ${PGHOST} $db
+    psql -h ${PGHOST} -d ${db} -f schema1.sql
 done
-dropdb comparisondatabase
+dropdb --if-exists comparisondatabase
 createdb comparisondatabase
 psql -d test2 -f schema2.sql
 
+export PGBINDIR=/usr/bin
+
+BASEURI=postgresql://$PGUSER@$PGHOST/
+
 # Dump out schema data for the two databases
-PGURI=postgresql://postgres@localhost/test1 PGCMPOUTPUT=/tmp/test-pgcmp-file1 PGCLABEL=db1 ../pgcmp-dump
-PGURI=postgresql://postgres@localhost/test2 PGCMPOUTPUT=/tmp/test-pgcmp-file2 PGCLABEL=db2 ../pgcmp-dump
+PGURI=${BASEURI}test1 PGCMPOUTPUT=/tmp/test-pgcmp-file1 PGCLABEL=db1 ../pgcmp-dump
+PGURI=${BASEURI}test2 PGCMPOUTPUT=/tmp/test-pgcmp-file2 PGCLABEL=db2 ../pgcmp-dump
 
 # Perform comparison
-PGURI=postgresql://postgres@localhost/comparisondatabase PGCMPINPUT1=/tmp/test-pgcmp-file1 PGCMPINPUT2=/tmp/test-pgcmp-file2 PGCEXPLANATIONS=./explanations.txt PGCLABEL1=db1 PGCLABEL2=db2 ../pgcmp
+PGURI=${BASEURI}comparisondatabase PGCMPINPUT1=/tmp/test-pgcmp-file1 PGCMPINPUT2=/tmp/test-pgcmp-file2 PGCEXPLANATIONS=./explanations.txt PGCLABEL1=db1 PGCLABEL2=db2 ../pgcmp
