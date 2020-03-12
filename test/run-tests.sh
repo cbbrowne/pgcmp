7ddf22811c713a70469d16105e4fe970891a4b80
cbbrowne <cbbrowne@cbbrowne.tor.int-afilias.info>
2020-03-12 16:56:04 -0400

    More UDS

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -6,14 +6,23 @@ echo "------------------------"
 export PGBINDIR=${PGBINDIR:-"/usr/bin"}
 export BASEURI="postgresql://$PGUSER@${PGHOST}:${PGPORT}/"
 export POST="?host=/var/run/postgresql"
+export comparison=comparisondatabase
 #export POST="?host=/tmp"
+T1URI=${BASEURI}test1${POST}
+T2URI=${BASEURI}test2${POST}
+CURI=${BASEURI}${comparison}${POST}
+
 echo "Configuration...
 
 PGHOST=${PGHOST}
 PGUSER=${PGUSER}
 BASEURI=${BASEURI}
 POST=${POST}
-PGBINDIR=${PGBINDIR}"
+PGBINDIR=${PGBINDIR}
+T1URI=${T1URI}
+T2URI=${T2URI}
+CURI=${CURI}
+"
 
 
 for db in test1 test2; do
@@ -21,13 +30,13 @@ for db in test1 test2; do
     createdb -h ${PGHOST} $db
     psql -h ${PGHOST} -d ${db} -f schema1.sql
 done
-dropdb --if-exists comparisondatabase
-createdb comparisondatabase
+dropdb --if-exists ${comparison}
+createdb ${comparison}
 psql -d test2 -f schema2.sql
 
 # Dump out schema data for the two databases
-PGURI=${BASEURI}test1${POST} PGCMPOUTPUT=/tmp/test-pgcmp-file1 PGCLABEL=db1 ../pgcmp-dump
-PGURI=${BASEURI}test2${POST} PGCMPOUTPUT=/tmp/test-pgcmp-file2 PGCLABEL=db2 ../pgcmp-dump
+PGURI=$TEST1URI PGCMPOUTPUT=/tmp/test-pgcmp-file1 PGCLABEL=db1 ../pgcmp-dump
+PGURI=$TEST2URI PGCMPOUTPUT=/tmp/test-pgcmp-file2 PGCLABEL=db2 ../pgcmp-dump
 
 # Perform comparison
-PGURI=${BASEURI}comparisondatabase${POST} PGCMPINPUT1=/tmp/test-pgcmp-file1 PGCMPINPUT2=/tmp/test-pgcmp-file2 PGCEXPLANATIONS=./explanations.txt PGCLABEL1=db1 PGCLABEL2=db2 ../pgcmp
+PGURI=$CURI PGCMPINPUT1=/tmp/test-pgcmp-file1 PGCMPINPUT2=/tmp/test-pgcmp-file2 PGCEXPLANATIONS=./explanations.txt PGCLABEL1=db1 PGCLABEL2=db2 ../pgcmp
